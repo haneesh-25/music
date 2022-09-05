affdc5e43718c47c913bd38dbd87750de6d6025d
haneesh-25 <67854450+haneesh-25@users.noreply.github.com>
2022-09-05 20:53:49 +0530

    Add files via upload

---
+++ emotion-based-music-ai-copy/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+import numpy as np
+import cv2
+from keras.models import load_model
+import webbrowser
+
+app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+info = {}
+
+haarcascade = "haarcascade_frontalface_default.xml"
+label_map = ['Anger', 'Neutral', 'Fear', 'Happy', 'Sad', 'Surprise']
+print("+"*50, "loadin gmmodel")
+model = load_model('model.h5')
+cascade = cv2.CascadeClassifier(haarcascade)
+
+
+@app.route('/')
+def emotion_detect():
+
+	found = False
+
+	cap = cv2.VideoCapture(0)
+	while not(found):
+		_, frm = cap.read()
+		gray = cv2.cvtColor(frm,cv2.COLOR_BGR2GRAY)
+
+		faces = cascade.detectMultiScale(gray, 1.4, 1)
+
+		for x,y,w,h in faces:
+			found = True
+			roi = gray[y:y+h, x:x+w]
+			cv2.imwrite("static/face.jpg", roi)
+
+	roi = cv2.resize(roi, (48,48))
+
+	roi = roi/255.0
+	
+	roi = np.reshape(roi, (1,48,48,1))
+
+	prediction = model.predict(roi)
+
+    print(prediction)
+
+	prediction = np.argmax(prediction)
+	prediction = label_map[prediction]
+
+    cap.release()
+
+
+
+	return render_template("music.html", emotion=prediction)
+
+    
+
+if __name__ == "__main__":
+	app.run(debug=True, host="192.168.43.238")
+
